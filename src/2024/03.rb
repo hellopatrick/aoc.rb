d2a0bf31f298ef8939eb7fa7c0d51e3192ec3f37
hellopatrick; Patrick Rogers <hellopatrick@users.noreply.github.com>
2024-12-02 21:22:29 -0800

    day 3

--- src/2024/03.rb
+++ src/2024/03.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
-_lines = ARGF.readlines
+re = /mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))/
+
+enabled = true
+part1 = 0
+part2 = 0
+
+ARGF.read.scan(re).each do |command|
+  a, b, on, = command
+
+  if a.nil?
+    enabled = !on.nil?
+    next
+  end
+
+  part1 += a.to_i * b.to_i
+  part2 += a.to_i * b.to_i if enabled
+end
+
+puts part1
+puts part2
