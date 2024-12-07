8ec21ff3458dd980c1a5bc4ef6a530a2f4f97210
hellopatrick; Patrick Rogers <hellopatrick@users.noreply.github.com>
2024-12-06 22:21:52 -0800

    answers for refactor

--- src/2024/07.rb
+++ src/2024/07.rb
@@ -19,8 +19,10 @@ def ok?(target, rest, allow_concat)
 
   return true if target % v == 0 && ok?(target / v, rest[..-2], allow_concat)
 
-  if allow_concat && target.to_s.end_with?(v.to_s) && ok?(target.to_s[...-v.to_s.size].to_i, rest[..-2], allow_concat)
-    return true
+  if allow_concat
+    vv = v.to_s
+    tt = target.to_s
+    return true if tt.end_with?(vv) && ok?(tt.chomp(vv).to_i, rest[..-2], allow_concat)
   end
 
   ok?(target - v, rest[..-2], allow_concat)
@@ -34,5 +36,5 @@ part2 = puzzle.filter_map do |n, vs|
   n if ok?(n, vs, true)
 end.sum
 
-puts part1
-puts part2
+raise "part1 wrong" unless part1 == 3_598_800_864_292
+raise "part2 wrong" unless part2 == 340_362_529_351_427
